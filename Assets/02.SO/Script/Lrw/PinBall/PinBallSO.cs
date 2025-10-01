08dca0f5bd59fe160bc8927370572d8b80f51431
riwoo0902 <imliu4342@gmail.com>
2025-10-01 11:44:36 +0900

    save

--- Assets/02.SO/Script/Lrw/PinBall/PinBallSO.cs
+++ Assets/02.SO/Script/Lrw/PinBall/PinBallSO.cs
@@ -10,10 +10,7 @@ namespace Lrw_PinBall
         [field: SerializeField] public float Mass { get; private set; } = 1;
         [field: SerializeField] public float Friction { get; private set; } = 0.2f;
         [field: SerializeField] public float Bounciness { get; private set; } = 0.5f;
-
         [field: SerializeField] public float BaseDamage { get; private set; } = 1;
-
-
     }
     public enum BallType
     {
