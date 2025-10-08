b92d9b419cf7759592126d2d37db120c632f85c6
riwoo0902 <imliu4342@gmail.com>
2025-10-08 22:04:56 +0900

    feat:Ord

--- Assets/01.Script/Lrw/PinBallMap/Ord.cs
+++ Assets/01.Script/Lrw/PinBallMap/Ord.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Lrw_Ord
 {
     public class Ord : MonoBehaviour
     {
-
+        [SerializeField] private UnityEvent OrdCollisionEvent;
 
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            
+            OrdCollisionEvent?.Invoke();
+            Destroy(gameObject);
         }
 
     }
