82e46de10774a95083e44df57ab8423747523df3
김도희 <dohuiasdf@gmail.com>
2025-10-30 10:29:17 +0900

    feat : PlayerStat

---
+++ Assets/01.Script/KDH/PlayerStatSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStatSO", menuName = "SOs/PlayerStatSO")]
+public class PlayerStatSO : ScriptableObject
+{
+    public float power;
+    public float health;
+}
