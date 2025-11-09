964e88f15e8d569c5233dea349073f969ee49fa4
김민 <pounl46v1@gmail.com>
2025-11-09 12:11:04 +0900

    Feat:BreackIntro

---
+++ Assets/01.Script/KM/Intro/Select.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[Serializable]
+public class ObjSaves
+{
+    public HingeJoint2D ChainLeft;
+    public HingeJoint2D ChainRight;
+}
+
+public class Select : MonoBehaviour
+{
+    [SerializeField] private LayerMask mask;
+    private GameObject NowSelect;
+    private GameObject arrow;
+    private Vector2 nowPos;
+    [SerializeField] private List<ObjSaves> chains = new List<ObjSaves>();
+    void FixedUpdate()
+    {
+        nowPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D collider = Physics2D.OverlapCircle(nowPos, 0.01f, mask);
+        if (collider)
+        {
+            Debug.Log(collider.gameObject.name);
+            NowSelect = collider.gameObject;
+        }
+        else
+        {
+            NowSelect = null;
+        }
+    }
+
+    private void Update()
+    {
+        if(Mouse.current.leftButton.isPressed && NowSelect != null)
+        {
+            if (NowSelect.CompareTag("Start"))
+            {
+                Destroy(chains[0].ChainLeft);
+                Destroy(chains[0].ChainRight);
+            }
+            else if (NowSelect.CompareTag("Setting"))
+            {
+                Destroy(chains[1].ChainLeft);
+                Destroy(chains[1].ChainRight);
+            }
+            else if (NowSelect.CompareTag("Exit"))
+            {
+                Destroy(chains[2].ChainLeft);
+                Destroy(chains[2].ChainRight);
+            }
+        }
+        if (NowSelect != null)
+        {
+            if (arrow)
+            {
+                arrow.transform.localScale = new Vector3(arrow.transform.localScale.x, 0, arrow.transform.localScale.z);
+                arrow = null;
+            }
+            arrow = NowSelect.transform.Find("Arrow").gameObject;
+            arrow.transform.localScale = new Vector3(arrow.transform.localScale.x, 1, arrow.transform.localScale.z);
+        }
+        else if (arrow != null)
+        {
+            arrow.transform.localScale = new Vector3(arrow.transform.localScale.x, 0, arrow.transform.localScale.z);
+            arrow = null;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(nowPos, 0.01f);
+    }
+}
