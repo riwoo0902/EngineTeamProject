31824f53072f4755b67b6be4822deb18b1eefffb
chohangyeol09 <198588682+chohangyeol09@users.noreply.github.com>
2025-11-17 17:43:16 +0900

    Update:

--- Assets/01.Script/KDH/PlayerActionTester.cs
+++ Assets/01.Script/KDH/PlayerActionTester.cs
@@ -1,31 +1,31 @@
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PlayerActionTester : MonoBehaviour
-{
-    [SerializeField] private PlayerStats playerStats;
-    [SerializeField] private List<ItemSO> testItems;  
-    [SerializeField] private ItemSO testItem;  
+//public class PlayerActionTester : MonoBehaviour
+//{
+//    [SerializeField] private PlayerStats playerStats;
+//    [SerializeField] private List<ItemSO> testItems;  
+//    [SerializeField] private ItemSO testItem;  
 
-    private void Start()
-    {
-        playerStats.action += OnInventoryChanged;
-        playerStats.AddItems(testItems);
-        playerStats.AddItem(testItem);
-    }
+//    private void Start()
+//    {
+//        playerStats.action += OnInventoryChanged;
+//        playerStats.AddItems(testItems);
+//        playerStats.AddItem(testItem);
+//    }
 
-    private void OnInventoryChanged(List<ItemSO> inventory)
-    {
-        Debug.Log($"인벤토리 변경 감지 현재 개수: {inventory.Count}");
-        foreach (var item in inventory)
-        {
-            Debug.Log($"{item.itemName}");
-        }
-    }
+//    private void OnInventoryChanged(List<ItemSO> inventory)
+//    {
+//        Debug.Log($"인벤토리 변경 감지 현재 개수: {inventory.Count}");
+//        foreach (var item in inventory)
+//        {
+//            Debug.Log($"{item.itemName}");
+//        }
+//    }
 
-    private void OnDestroy()
-    {
-        if (playerStats != null)
-            playerStats.action -= OnInventoryChanged;
-    }
-}
+//    private void OnDestroy()
+//    {
+//        if (playerStats != null)
+//            playerStats.action -= OnInventoryChanged;
+//    }
+//}
