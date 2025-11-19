504e825c530d22105012c4d057cb32a4779b1e73
riwoo0902 <imliu4342@gmail.com>
2025-11-19 11:42:45 +0900

    feat:Slot

---
+++ Assets/01.Script/Lrw/UI/PinBalls/PinballSlotBase.cs
@@ -0,0 +1,43 @@
+using System;
+using Lrw_PinBall;
+using UnityEngine;
+
+namespace _01.Script.Lrw.UI.PinBalls
+{
+    public abstract class PinballSlotBase : MonoBehaviour, IPinBallSlot
+    {
+        [field:SerializeField] public PinBallSO Pinball { get; set; }
+        private SpriteRenderer _spriteRenderer;
+        [SerializeField] protected PinBallUISetting pinBallUISetting;
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        
+        public void SetPinBall(PinBallSO pinball)
+        {
+            Pinball =  pinball;
+            _spriteRenderer.sprite = Pinball.PinBallImage;
+            SettingPinBallUISetting();
+        }
+
+        private void SettingPinBallUISetting()
+        {
+            pinBallUISetting.pinBallName = Pinball.name;
+            pinBallUISetting.pinBallExplanation = Pinball.BallExplanation;
+            pinBallUISetting.pinBallBounce = Pinball.Bounciness.ToString();
+            pinBallUISetting.pinBallFriction = Pinball.Friction.ToString();
+            pinBallUISetting.pinBallMass = Pinball.Mass.ToString();
+        }
+    }
+    
+    [Serializable]
+    public class PinBallUISetting
+    {
+        public string pinBallName;
+        public string pinBallExplanation;
+        public string pinBallBounce;
+        public string pinBallFriction;
+        public string pinBallMass;
+    }
+}
