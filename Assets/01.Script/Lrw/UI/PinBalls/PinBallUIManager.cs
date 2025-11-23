0c4e43bc59e9604ad4036b25003a5df363e51443
riwoo0902 <imliu4342@gmail.com>
2025-11-23 20:32:38 +0900

    fix:PinBallSpawn

--- Assets/01.Script/Lrw/UI/PinBalls/PinBallUIManager.cs
+++ Assets/01.Script/Lrw/UI/PinBalls/PinBallUIManager.cs
@@ -74,6 +74,7 @@ namespace _01.Script.Lrw.UI.PinBalls
         [ContextMenu("Reset PinBalls")]
         public void ReSet()
         {
+            PinBallSpawner.Instance.SetCanSpawn(true);
             CurrentHavePinBalls = PinballInventory.Instance.inventory.ToArray().ToList();
             PinBallSpawner.Instance.PinBallSpawn();
         }
