94c78b788cb33b585192dd19b9e66cdc871c21ea
riwoo0902 <imliu4342@gmail.com>
2025-11-23 21:05:33 +0900

    save

--- Assets/01.Script/CHG/BattleStage/BattleTurnManager.cs
+++ Assets/01.Script/CHG/BattleStage/BattleTurnManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using _01.Script.Lrw.PinBallMap;
 using UnityEngine;
 
 public class BattleTurnManager : MonoBehaviour
@@ -32,7 +33,7 @@ public class BattleTurnManager : MonoBehaviour
     public void PlayerTurnSet()
     {
         StartCoroutine(ChangePlayerTurn());
-
+        PinBallSpawner.Instance.SetCanSpawn(true);
     }
 
     private IEnumerator ChangePlayerTurn()
--- Assets/01.Script/Lrw/UI/PinBalls/PinBallUIManager.cs
+++ Assets/01.Script/Lrw/UI/PinBalls/PinBallUIManager.cs
@@ -74,7 +74,6 @@ namespace _01.Script.Lrw.UI.PinBalls
         [ContextMenu("Reset PinBalls")]
         public void ReSet()
         {
-            PinBallSpawner.Instance.SetCanSpawn(true);
             CurrentHavePinBalls = PinballInventory.Instance.inventory.ToArray().ToList();
             PinBallSpawner.Instance.PinBallSpawn();
         }
