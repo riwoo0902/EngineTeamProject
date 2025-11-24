0bdebccb92c37ab8431a36e2f485c67f92cc5479
chohangyeol09 <198588682+chohangyeol09@users.noreply.github.com>
2025-11-24 11:07:15 +0900

    aaaa

--- Assets/01.Script/CHG/C BattleLoad.cs
+++ Assets/01.Script/CHG/C BattleLoad.cs
@@ -4,9 +4,9 @@ public class CBattleLoad : MonoBehaviour
 {
     public void ButtonClick()
     {
+        StageManager.Instance.SceneChange(MapType.Boss);
         //StageManager.Instance.SceneChange(MapType.Event);
         //StageManager.Instance.SceneChange(MapType.Store);
         //StageManager.Instance.SceneChange(MapType.Battle);
-        StageManager.Instance.SceneChange(MapType.Boss);
     }
 }
